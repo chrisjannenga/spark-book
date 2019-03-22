488a3fb200bdc56469007e2e656aafb341a0ac01
Chris Jannenga <cjannenga312@gmail.com>
2019-03-22 13:00:35 -0700

    More stuff

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
 // tslint:disable-next-line: max-line-length
-    new Recipe('A Test Recipe', 'This is just a test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg')
+    new Recipe('A Test Recipe', 'This is just a test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'),
+    new Recipe('Second Recipe', 'I am the second test Recipe', 'https://static01.nyt.com/images/2016/05/03/dining/03COOKING-PASTAWITHBUTTER2/03COOKING-PASTAWITHBUTTER2-videoSixteenByNineJumbo1600.jpg')
   ];
 
   constructor() { }
